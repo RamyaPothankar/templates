8e7d0f7333030cf6e2df5104c92957d12341e5d6
RamyaPothankar <ramya.pothankar@cognizant.com>
2026-02-06 12:54:31 +0530

    Update idx-template.nix to create Angular project

--- angular/idx-template.nix
+++ angular/idx-template.nix
@@ -2,18 +2,25 @@
   channel = "stable-24.11";
   packages = [ pkgs.nodejs_20 ];
   bootstrap = ''
+    # Create new angular project
     npx --prefer-offline -y @angular/cli new --skip-git --defaults --skip-install --directory "$WS_NAME" "$WS_NAME"
+    
+    # Create .idx dir and copy configs
     mkdir "$WS_NAME"/.idx
     cp ${./dev.nix} "$WS_NAME"/.idx/dev.nix && chmod +w "$WS_NAME"/.idx/dev.nix
-    mv "$WS_NAME" "$out"
+    cp -rf ${./.idx/airules.md} "$WS_NAME"/.idx/airules.md
+    cp -rf ${./.idx/eslint.config.js} "$WS_NAME"/.idx/eslint.config.js
+    cp -rf ${./.idx/update-pkg.js} "$WS_NAME"/.idx/update-pkg.js
     
-    mkdir -p "$out/.idx"
-
+    # Move project to final location
+    mv "$WS_NAME" "$out"
     chmod -R u+w "$out"
-    cp -rf ${./.idx/airules.md} "$out/.idx/airules.md"
+    
+    # Copy GEMINI.md
     cp -rf "$out/.idx/airules.md" "$out/GEMINI.md"
-    chmod -R u+w "$out"
 
-    (cd "$out"; npm install --package-lock-only --ignore-scripts)
+    # Run update script and install dependencies
+    (cd "$out"; node ./.idx/update-pkg.js; rm ./.idx/update-pkg.js; npm install --package-lock-only --ignore-scripts)
   '';
 }
+
