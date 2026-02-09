6099a9dae75f2baa4a418f46862b25863a1cc9d5
RamyaPothankar <ramya.pothankar@cognizant.com>
2026-02-09 13:26:46 +0530

    Integrate ESLint and Prettier in project template
    Added ESLint and Prettier integration to the project setup.

--- svelte-vite/idx-template.nix
+++ svelte-vite/idx-template.nix
@@ -5,6 +5,29 @@
   bootstrap = ''
     mkdir -p "$WS_NAME"
     npm create -y vite@latest "$WS_NAME" -- --template ${if language == "ts" then "svelte-ts" else "svelte"}
+
+    # --- Start of ADDED ESLint/Prettier Integration ---
+    # Enter the new project directory to configure it.
+    cd "$WS_NAME"
+
+    # Remove the default ESLint config created by Vite, if it exists.
+    rm -f ./.eslintrc.cjs
+
+    # Copy our custom ESLint config from the template's .idx directory.
+    cp -f ${./.idx/eslint.config.js} ./eslint.config.js
+
+    # Copy and run the helper script to update package.json with ESLint/Prettier dependencies.
+    cp -f ${./.idx/update-pkg.cjs} ./update-pkg.cjs
+    node ./update-pkg.cjs
+    rm ./update-pkg.cjs # Clean up the script after use.
+
+    # Now, install all dependencies (including the new ones).
+    npm install --ignore-scripts
+
+    # Go back to the original directory to finalize the template setup.
+    cd ..
+    # --- End of ADDED ESLint/Prettier Integration ---
+
     mkdir -p "$WS_NAME/.idx/"
     cp -rf ${./icon.png} "$WS_NAME/.idx/icon.png"
     cp -rf ${./dev.nix} "$WS_NAME/.idx/dev.nix"
