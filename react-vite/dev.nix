5f92701daee93b35123bef7f940ff5a2546493b5
RamyaPothankar <ramya.pothankar@cognizant.com>
2026-02-12 13:06:37 +0530

    Update dev.nix

--- react-vite/dev.nix
+++ react-vite/dev.nix
@@ -5,14 +5,17 @@
   channel = "stable-24.05"; # or "unstable"
   # Use https://search.nixos.org/packages to find packages
   packages = [
-    pkgs.nodejs_20
+    pkgs.nodejs_20,
+    pkgs.nodePackages.eslint,
+    pkgs.nodePackages.prettier
   ];
   # Sets environment variables in the workspace
   env = {};
   idx = {
     # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
     extensions = [
-      # "vscodevim.vim"
+      "dbaeumer.vscode-eslint",
+      "esbenp.prettier-vscode"
     ];
     workspace = {
       # Runs when a workspace is first created with this `dev.nix` file
@@ -35,3 +38,4 @@
     };
   };
 }
+
