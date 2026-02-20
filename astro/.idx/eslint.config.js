1631a11c990f11c02575116bd7660d493f9e2cc8
RamyaPothankar <ramya.pothankar@cognizant.com>
2026-02-20 17:39:10 +0530

    Refactor ESLint configuration for Astro project

--- astro/.idx/eslint.config.js
+++ astro/.idx/eslint.config.js
@@ -1,19 +1,12 @@
-import eslintPluginAstro from 'eslint-plugin-astro';
+import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
+import astro from 'eslint-plugin-astro';
 import prettier from 'eslint-config-prettier';
 
 export default [
-  // 1. Global ignores
-  {
-    ignores: ['dist', 'node_modules', '*.cjs', '**/*.config.js'],
-  },
-
-  // 2. Recommended rules from typescript-eslint, applied to all .ts files
+  eslint.configs.recommended,
   ...tseslint.configs.recommended,
-
-  // 3. Astro rules
-  ...eslintPluginAstro.configs.all,
-
-  // 4. Prettier config must be last.
+  ...astro.configs['flat/recommended'],
   prettier,
 ];
+
